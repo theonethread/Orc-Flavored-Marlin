c9eedec2276902b0cecfdce29d83231e260e4327
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2022-04-26 14:57:11 +0200

    Orc v1.0 updates + documentation

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,6 +993,8 @@
  */
 #define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
+  //#define DEFAULT_XJERK 10.0
+  //#define DEFAULT_YJERK 10.0
   #define DEFAULT_XJERK  8.0
   #define DEFAULT_YJERK  8.0
   #define DEFAULT_ZJERK  0.3
